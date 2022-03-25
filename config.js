8dfee1c442f8ddbb8c2b93d1bbe0ce452961876b
骆邯 <58976235@qq.com>
2022-03-25 16:23:17 +0800

    静态资源打包

---
+++ config.js
@@ -0,0 +1,19 @@
+/*
+ * @Author: your name
+ * @Date: 2022-03-25 14:58:09
+ * @LastEditTime: 2022-03-25 15:04:07
+ * @LastEditors: Please set LastEditors
+ * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ * @FilePath: /gulp-template/config.js
+ */
+// 项目名称
+const projectName = 'jiwu_test'
+
+// 静态资源服务器
+const baseUrl = 'http://192.168.100.195:8888/'
+
+
+module.exports = {
+    projectName,
+    baseUrl
+}
--- gulpfile.js
+++ gulpfile.js
@@ -1,171 +1,203 @@
 const gulp = require('gulp')
+const gulpif = require('gulp-if');
+const minimist = require('minimist');
 const gulpLess = require('gulp-less');
-const autoprefixer=require('gulp-autoprefixer')
+const autoprefixer = require('gulp-autoprefixer')
 const cssmin = require('gulp-cssmin')
-const uglify=require('gulp-uglify')
-const babel=require('gulp-babel')
-const htmlmin = require('gulp-htmlmin')
-const del=require('del')
-const webserver =require('gulp-webserver')
+const uglify = require('gulp-uglify')
+const babel = require('gulp-babel')
+const del = require('del')
+const webserver = require('gulp-webserver')
 const pxtorem = require('gulp-pxtorem');
-const rename = require('gulp-rename'); 
-const htmltpl = require('gulp-html-tpl')
-const artTemplate = require('art-template')
+const rename = require('gulp-rename');
 const resolvePath = require("gulp-resolve-path");
 const through = require('through2')
+const { projectName, baseUrl } = require("./config")
 const options = {
-    /*
-     root: process.cwd(),
-     ext: {
-     template: ['html'],
-     script: ['js'],
-     style: ['css', 'less', 'sass']
-     }
-     */
+  /*
+   root: process.cwd(),
+   ext: {
+   template: ['html'],
+   script: ['js'],
+   style: ['css', 'less', 'sass']
+   }
+   */
 };
+
+const knownOptions = {
+  string: 'env',
+  default: { env: process.env.NODE_ENV || 'production' }
+};
+
+const gulpOptions = minimist(process.argv.slice(2), knownOptions);
+
 const pxtoremOptions = {
-    rootValue: 32,
-    unitPrecision: 6,
-    propList: ['*'],
-    selectorBlackList: [],
-    replace: true,
-    mediaQuery: false,
-    minPixelValue: 2,
-    exclude: /node_modules/i
+  rootValue: 32,
+  unitPrecision: 6,
+  propList: ['*'],
+  selectorBlackList: [],
+  replace: true,
+  mediaQuery: false,
+  minPixelValue: 2,
+  exclude: /node_modules/i
 }
 const postcssOptions = {
-    processors: [
-        autoprefixer({ overrideBrowserslist: ['> 0.15% in CN'] })
-    ]
+  processors: [
+    autoprefixer({ overrideBrowserslist: ['> 0.15% in CN'] })
+  ]
 }
 
-const lessHandler=function(){
-    return gulp
-    .src(['./src/public/less/*.less','./src/pages/**/*.less','./src/components/**/*.less'])
+const lessHandler = function () {
+  return gulp
+    .src(['./src/public/less/*.less', './src/pages/**/*.less', './src/components/**/*.less'])
     .pipe(gulpLess({
-        javascriptEnabled: true
+      javascriptEnabled: true
     }))
     .pipe(autoprefixer())
     .pipe(pxtorem(pxtoremOptions, postcssOptions))
     .pipe(resolvePath(options))
     .pipe(through.obj(function (chunk, enc, cb) {
-        let contents=chunk.contents.toString()
-        let newContents=contents.replace(/\/src\/static/g,'../')
-        chunk.contents=Buffer.from(newContents)
-        /* let fileName=chunk.relative.split('/')[0]
-        let contents=chunk.contents.toString()
-        let newContents=contents.replace('/src/pages/'+fileName+'/'+fileName+'.less','./css/'+fileName+'.css')
-        newContents=newContents.replace(/\/src\/public\/js/g,'./js')
-        newContents=newContents.replace('/src/pages/'+fileName+'/'+fileName+'.js','./js/'+fileName+'.js')
-        newContents=newContents.replace(/\/src\/static/g,'.')
-        chunk.contents=Buffer.from(newContents) */
-        cb(null,chunk)
-      }))
-    .pipe(cssmin())
-    .pipe(rename({dirname:''}))
+      let contents = chunk.contents.toString()
+      let newContents = contents
+      if (gulpOptions.env === 'production') {
+        let img = baseUrl + 'images/' + projectName + '/'
+        let font = baseUrl + 'fonts/' + projectName + '/'
+        //newContents = contents.replace(/\/src\/static/g, '..')
+        newContents = newContents.replace(/\\src\\static\\images\\/g, img)
+        newContents = newContents.replace(/\\src\\static\\font\\/g, font)
+      } else {
+        //newContents = contents.replace(/\/src\/static/g, '..')
+        newContents = newContents.replace(/\\src\\static\\images\\/g, '../images/')
+        newContents = newContents.replace(/\\src\\static\\font\\/g, '../font/')
+      }
+      chunk.contents = Buffer.from(newContents)
+      cb(null, chunk)
+    }))
+    .pipe(gulpif(gulpOptions.env === 'production', cssmin()))
+    .pipe(rename({ dirname: '' }))
     .pipe(gulp.dest('./m/css'))
 }
 
-const cssHandler=function(){
-    return gulp
-        .src('./src/static/css/*.css')
-        .pipe(cssmin())
-        .pipe(rename({dirname:''}))
-        .pipe(gulp.dest('./m/css'))
+const cssHandler = function () {
+  return gulp
+    .src('./src/static/css/*.css')
+    .pipe(gulpif(gulpOptions.env === 'production', cssmin()))
+    .pipe(rename({ dirname: '' }))
+    .pipe(gulp.dest('./m/css'))
 }
 
-const jsHandler=function(){
-    return gulp
-    .src(['./src/public/js/*.js','./src/pages/**/*.js','./src/components/**/*.js'])
-    .pipe(babel({
-        presets:['@babel/env']
-    }))
-    .pipe(uglify())
-    .pipe(rename({dirname:''}))
+const jsHandler = function () {
+  return gulp
+    .src(['./src/pages/**/*.js', './src/components/**/*.js'])
+    .pipe(gulpif(gulpOptions.env === 'production', babel({
+      presets: ['@babel/env']
+    })))
+    .pipe(gulpif(gulpOptions.env === 'production', uglify()))
+    .pipe(rename({ dirname: '' }))
     .pipe(gulp.dest('./m/js'))
 }
 
-const htmlHandler=function(){
-    return gulp
+const publicJsHandler = function () {
+  return gulp
+    .src(['./src/public/js/*.js'])
+    .pipe(rename({ dirname: '' }))
+    .pipe(gulp.dest('./m/js'))
+}
+
+const htmlHandler = function () {
+  return gulp
     .src('./src/pages/**/*.html')
     .pipe(resolvePath(options))
     .pipe(through.obj(function (chunk, enc, cb) {
-        let fileName=chunk.relative.split('/')[0]
-        let contents=chunk.contents.toString()
-        let newContents=contents.replace('/src/pages/'+fileName+'/'+fileName+'.less','./css/'+fileName+'.css')
-        newContents=newContents.replace(/\/src\/public\/js/g,'./js')
-        newContents=newContents.replace('/src/pages/'+fileName+'/'+fileName+'.js','./js/'+fileName+'.js')
-        newContents=newContents.replace(/\/src\/static/g,'.')
-        chunk.contents=Buffer.from(newContents)
-        cb(null,chunk)
-      }))
-    .pipe(htmlmin({
-        collapseWhitespace: true,
-        removeComments: true, //删除注释
-        collapseBooleanAttributes: true,  //省略布尔属性的值 <input checked="true"/> ==> <input checked />
-        removeEmptyAttributes: true,  //删除所有空格作属性值 <input id="" /> ==> <input />
-        removeScriptTypeAttributes: true,  //删除<script>的type="text/javascript"
-        removeStyleLinkTypeAttributes: true,  //删除<style>和<link>的type="text/css"
-        minifyJS: true,  //压缩页面JS
-        minifyCSS: true  //压缩页面CSS
+      let arr = chunk.relative.split('/').length > 1 ? chunk.relative.split('/') : chunk.relative.split('\\')
+      let fileName = arr[0]
+      let contents = chunk.contents.toString()
+      let newContents = contents
+
+      if (gulpOptions.env === 'production') {
+        let css = baseUrl + 'css/' + projectName + '/'
+        let js = baseUrl + 'js/' + projectName + '/'
+        let img = baseUrl + 'images/' + projectName + '/'
+        newContents = contents.replace('/src/pages/' + fileName + '/' + fileName + '.less', css + fileName + '.css')
+        newContents = newContents.replace('\\src\\pages\\' + fileName + '\\' + fileName + '.less', css+ fileName + '.css')
+        newContents = newContents.replace('/src/static/css/', css)
+        newContents = newContents.replace('\\src\\static\\css\\', css)
+        newContents = newContents.replace(/\/src\/public\/js\//g, js)
+        newContents = newContents.replace(/\\src\\public\\js\\/g, js)
+        newContents = newContents.replace('/src/pages/' + fileName + '/' + fileName + '.js', js + fileName + '.js')
+        newContents = newContents.replace('\\src\\pages\\' + fileName + '\\' + fileName + '.js', js + fileName + '.js')
+        newContents = newContents.replace(/\/src\/static\/images\//g, img)
+        newContents = newContents.replace(/\\src\\static\\images\\/g, img)
+      } else {
+        newContents = contents.replace('/src/pages/' + fileName + '/' + fileName + '.less', './css/' + fileName + '.css')
+        newContents = newContents.replace('\\src\\pages\\' + fileName + '\\' + fileName + '.less', './css/' + fileName + '.css')
+        newContents = newContents.replace('/src/static/css/', './css/')
+        newContents = newContents.replace('\\src\\static\\css\\', './css/')
+        newContents = newContents.replace(/\/src\/public\/js/g, './js')
+        newContents = newContents.replace('/src/pages/' + fileName + '/' + fileName + '.js', './js/' + fileName + '.js')
+        newContents = newContents.replace(/\/src\/static/g, '.')
+        newContents = newContents.replace(/\\src\\public\\js\\/g, './js/')
+        newContents = newContents.replace('\\src\\pages\\' + fileName + '\\' + fileName + '.js', './js/' + fileName + '.js')
+        newContents = newContents.replace(/\\src\\static\\/g, './')
+        newContents = newContents.replace('../../components/', '')
+        newContents = newContents.replace('..\\..\\components\\', '')
+        
+      }
+      chunk.contents = Buffer.from(newContents)
+      cb(null, chunk)
     }))
-    .pipe(htmltpl({
-        tag:'template',
-        paths:['./src/components'],
-        engine:function(template,data){
-            return template && artTemplate.compile(template)(data)
-        },
-        data:{
-            header:false,
-            footer:false
-        }
-    }))
-    .pipe(rename({dirname:''}))
+    .pipe(rename({ dirname: '' }))
     .pipe(gulp.dest('./m'))
 }
 
-const imgHandler=function(){
-    return gulp
+const imgHandler = function () {
+  return gulp
     .src('./src/static/images/**')
     .pipe(gulp.dest('./m/images'))
 }
 
-const fontHandler=function(){
-    return gulp
+const fontHandler = function () {
+  return gulp
     .src('./src/static/font/**')
     .pipe(gulp.dest('./m/font'))
 }
 
-const delHandler=function(){
-    return del(['./m'])
+const delHandler = function () {
+  return del(['./m'])
 }
 
-const webHandler=function(){
-    return gulp
+const webHandler = function () {
+  return gulp
     .src('./m')
     .pipe(webserver({
-        host:'localhost',
-        port:'8080',
-        livereload:true,  
-        open:'./index.html',
-        proxies:[]
+      host: 'localhost',
+      port: '8081',
+      livereload: true,
+      open: './index.html',
+      /* proxies:[
+          {
+              source: '/api', target: 'http://localhost:3000'  //后端地址
+          }
+      ] */
     }))
 }
 
-const watchHandler=function(){
-    gulp.watch('./src/public/less/*.less',lessHandler),
-    gulp.watch('./src/pages/**/*.less',lessHandler),
-    gulp.watch('./src/components/**/*.less',lessHandler),
-    gulp.watch('./src/static/css/*.css',cssHandler),
-    gulp.watch('./src/public/js/*.js',jsHandler),
-    gulp.watch('./src/pages/**/*.js',jsHandler),
-    gulp.watch('./src/components/**/*.js',jsHandler),
-    gulp.watch('./src/pages/**/*.html',htmlHandler),
-    gulp.watch('./src/components/**/*.html',htmlHandler)
+const watchHandler = function () {
+  gulp.watch('./src/public/less/*.less', lessHandler),
+    gulp.watch('./src/pages/**/*.less', lessHandler),
+    gulp.watch('./src/components/**/*.less', lessHandler),
+    gulp.watch('./src/static/css/*.css', cssHandler),
+    gulp.watch('./src/pages/**/*.js', jsHandler),
+    gulp.watch('./src/public/js/*.js', publicJsHandler),
+    gulp.watch('./src/components/**/*.js', jsHandler),
+    gulp.watch('./src/pages/**/*.html', htmlHandler),
+    gulp.watch('./src/components/**/*.html', htmlHandler)
 }
-module.exports.default=gulp.series(
-    delHandler,
-    gulp.parallel(lessHandler,jsHandler,htmlHandler,imgHandler,fontHandler,cssHandler),
-    webHandler,
-    watchHandler
+
+
+exports.default = gulp.series(
+  delHandler,
+  gulp.parallel(lessHandler, publicJsHandler, jsHandler, htmlHandler, imgHandler, fontHandler, cssHandler),
+  webHandler,
+  watchHandler
 )
